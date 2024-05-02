362e9885e355295c0b8d41d4de923fd2514a1542
tommi <95100445+tommi3141@users.noreply.github.com>
2024-05-02 18:19:39 +0000

    added all files generated from nicepage

---
+++ scripts/form-carousel_3884.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Request from yeyymathe.tk',
+    'email_message' => 'Das ist ein Test',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'yeyymathe@posteo.de',
+    'to' => 'yeyymathe@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
